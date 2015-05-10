1a804c754da2b8da871f92398097d43f803021a3
Ben Reiter <benkreiter@gmail.com>
2015-05-09 21:38:49 -0700

    start of an audio streaming app

---
+++ audio_app.py
@@ -0,0 +1,32 @@
+import pyaudio
+import numpy
+import time
+
+WIDTH = 2
+CHANNELS = 2
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+def callback(in_data, frame_count, time_info, status):
+    return (in_data, pyaudio.paContinue)
+
+stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                input_device_index = 2,
+                output_device_index = 1,                
+                output=True,
+                stream_callback=callback)
+
+stream.start_stream()
+
+while stream.is_active():
+    time.sleep(0.1)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
