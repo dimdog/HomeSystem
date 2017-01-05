6d75699a476cb90002cc96ed3d79a2fc0da1f9ca
Benjamin Reiter <ben@wake.io>
2017-01-05 00:01:38 -0800

    trying to allow picking the affected led

--- static/js/tiler.js
+++ static/js/tiler.js
@@ -6,7 +6,9 @@ $('#picker').colpick({
   colorScheme:'dark',
   onChange:function(hsb,hex,rgb,el,bySetColor) {
     $(el).css('border-color','#'+hex);
-    $.get( "http://10.0.1.2/color?red="+rgb.r+"&green="+rgb.g+"&blue="+rgb.b+"&position=5");
+    var led_number = document.getElementById('led_number').value;
+    console.log(led_number);
+    $.get( "http://10.0.1.18/color?red="+rgb.r+"&green="+rgb.g+"&blue="+rgb.b+"&position="+led_number);
 
     // Fill the text box just if the color was set using the picker, and not the colpickSetColor function.
     if(!bySetColor) $(el).val(hex);
@@ -15,5 +17,5 @@ $('#picker').colpick({
   $(this).colpickSetColor(this.value);
 });
 function power(){
-  $.get( "http://10.0.1.2/power");
+  $.get( "http://10.0.1.18/power");
 }
